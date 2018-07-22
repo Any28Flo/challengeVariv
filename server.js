c189f548fb80c4122b23911a58de1c407b8da8cf
Analin Flores <foglzerika@gmail.com>
2018-07-22 01:53:22 -0500

    Adding express

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var starWarsApp = express();
+const express = require("express");
+const challengeVariv = express();
 
-starWarsApp.use("/static", express.static(__dirname+'/assets'));
+challengeVariv.use("/static", express.static(__dirname+'/assets'));
 
-starWarsApp.get('/',function(req,res){
+challengeVariv.get('/',function(req,res){
 	res.sendFile(__dirname+'/index.html');
-	console.log("Servidor en funcionamiento");
+	console.log("Server on");
 })
 
 starWarsApp.listen(8080);
