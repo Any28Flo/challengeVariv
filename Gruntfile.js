df9a4fbb15eb45bc25506a81ea2f809f6a87ef66
Analin Flores <foglzerika@gmail.com>
2018-07-21 21:29:26 -0500

    Adding Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+        watch: {
+            files: '*.css'
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                      'assets/css/*.css',
+                      'index.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: ''
+                }
+            }
+          }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+    grunt.registerTask('default', [ 'browserSync' , 'watch']);
+};
